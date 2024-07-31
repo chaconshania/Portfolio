3c4a9b4ca546316551f6dd84b329a0a982efb31e
Shania <chacon1shania@gmail.com>
2024-07-31 12:42:09 -0400

    Updated CSS. Made images responsive. and same width

--- src/Sections/Projects/Projects.jsx
+++ src/Sections/Projects/Projects.jsx
@@ -7,7 +7,7 @@ import weatherApp from "../../assets/weather-app.png";
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-      <h1 className="sectionTitle">Projects</h1>
+      <h1 className="sectionTitle">Featured Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
           src={webpage}
