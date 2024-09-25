041856d6ee2c09de3eb0bc7b4f31d2b5990b6751
Shania <chacon1shania@gmail.com>
2024-09-25 00:57:20 -0400

    Designed the about page

--- src/webpages/About/Bio/Bio.jsx
+++ src/webpages/About/Bio/Bio.jsx
@@ -1,35 +1,31 @@
 import React from "react";
 import styles from "./BioStyles.module.css";
-import aboutImg from "../../../assets/hero-img-light.png";
+import profileImg from "../../../assets/hero-img-light.png";
+import profileDark from "../../../assets/hero-img-dark.png";
 function Bio() {
   return (
     <section id="hero" className={styles.container}>
-      <div className={styles.about}>
-        <div>
-          <h1 className="sectionTitle">About me</h1>
-          <p className={styles.description}>
-            Based in CT, I am currently a student at the University of
-            Connecticut for a BFA in Digital Media Design, with a concentration
-            in Web/Interactive Media Design. I am passionate about web
-            development and proficient in front-end technologies such as HTML5,
-            CSS, JavaScript, React, and Bootstrap. <br /> <br />I love to travel
-            and draw whatever comes in mind, and I take every opportunity to
-            take pictures of my friends and any adorable animal!
-          </p>
-        </div>
-        <div>
-          <h1 className="sectionTitle">About me</h1>
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.hero}
+          src={profileImg}
+          id="heroImg"
+          alt="Profile picture of myself"
+        />
+      </div>
+      <div className={styles.info}>
+        <span className={styles.circle}></span>
+        <h1>About Me</h1>
 
-          <p className={styles.description}>
-            Based in CT, I am currently a student at the University of
-            Connecticut for a BFA in Digital Media Design, with a concentration
-            in Web/Interactive Media Design. I am passionate about web
-            development and proficient in front-end technologies such as HTML5,
-            CSS, JavaScript, React, and Bootstrap. <br /> <br />I love to travel
-            and draw whatever comes in mind, and I take every opportunity to
-            take pictures of my friends and any adorable animal!
-          </p>
-        </div>
+        <p className={styles.description}>
+          Based in CT, I am currently a student at the University of Connecticut
+          for a BFA in Digital Media Design, with a concentration in
+          Web/Interactive Media Design. I am passionate about web development
+          and proficient in front-end technologies such as HTML5, CSS,
+          JavaScript, React, and Bootstrap. <br /> <br />I love to travel and
+          draw whatever comes in mind, and I take every opportunity to take
+          pictures of my friends and any adorable animal!
+        </p>
       </div>
     </section>
   );
