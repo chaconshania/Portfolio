6dbb7110a48f48edd3344810ae6155a97541bbc3
Shania <chacon1shania@gmail.com>
2024-10-05 23:56:36 -0400

    switched the link from the card to the button instead

--- src/common/Project.jsx
+++ src/common/Project.jsx
@@ -1,17 +1,17 @@
 import styles from "./ProjectStyles.module.css";
 function Project({ src, link, h3, p, h6 }) {
   return (
-    <a href={link} target="_blank" rel="noopener noreferrer">
-      <div className={styles.projectCard}>
-        <img src={src} alt="Project visual" />
-        <div className={styles.cardBody}>
-          <h6>{h6}</h6>
-          <h3>{h3}</h3>
-          <p>{p}</p>
+    <div className={styles.projectCard}>
+      <img src={src} alt="Project visual" />
+      <div className={styles.cardBody}>
+        <h6>{h6}</h6>
+        <h3>{h3}</h3>
+        <p>{p}</p>
+        <a href={link} target="_blank" rel="noopener noreferrer">
           <button>View</button>
-        </div>
+        </a>
       </div>
-    </a>
+    </div>
   );
 }
 
